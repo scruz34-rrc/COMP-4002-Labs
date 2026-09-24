1f28d4928685b232aa8a0c0b650d8d9397c70d30
Silverio Cruz <scruz34-rrc>
2026-09-23 21:40:45 -0500

    Added page header.

---
+++ src/components/header.tsx
@@ -0,0 +1,16 @@
+export default function Header() {
+    return (
+        <header className="site-header">
+            <div className="header-left">
+                <img
+                    src="/images/logo.svg"
+                    alt="Pixell River Financial logo"
+                    className="logo"
+                />
+            </div>
+            <div className="header-text">
+                <h1>Pixell River Employee Directory</h1>
+            </div>
+        </header>
+    );
+}
